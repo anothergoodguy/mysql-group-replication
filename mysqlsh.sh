1ef9169f06c2f84e67cae25dee8d7492145d115a
anothergoodguy <mvallapuneni@gmail.com>
2023-03-12 03:05:57 +0530

    initial version

---
+++ mysqlsh.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+source ./.env
+
+docker-compose \
+    exec \
+    server-1 \
+    mysqlsh \
+    --user=${MYSQL_USER} \
+    --password=${MYSQL_PASSWORD} \
+    --schema="mysql" \
+    "$@"
